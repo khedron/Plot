d50ba1bd5d1be1df4d4b4482eb54fe1490ebac03
John Wye <john.wye@gmail.com>
2009-09-17 17:51:41 +0100

    Updated properties to link inner and outer changed() signals

--- base/property.py
+++ base/property.py
@@ -1,20 +1,91 @@
 from PyQt4.QtCore import pyqtSignal, pyqtProperty, QObject
 
-def get_fget(mem, type, default):
+"""
+pyqtSignal and pyqtProperty convenience functions.
+
+These functions try to simplify signal/property pair creation and linking.
+They are intended to be used like so:
+
+	class Person(QObject):
+		changed = pyqtSignal()
+
+		# name defaults to "Joe Bloggs"
+		name, name_changed = prop_sig(str, "name", "Joe Bloggs")
+		# age defaults to int(), i.e. 0
+		age, age_changed = prop_sig(int, "age")
+		<var>, <var>_changed = prop_sig(<type>, <var-as-string>,[<default>])
+
+		def __init__(self):
+			QObject.__init__(self)
+			for signal in self.name_changed, self.age_changed, self.<var>_changed:
+				signal.connect(self.changed)
+
+This code creates a Person class with two properties: name and age. When these
+properties are changed, they emit the name_changed and age_changed signals,
+respectively.
+
+These signals need to be manually connected up to the class's changed() signal.
+This is because if a containing class defines a Person property/signal pair
+with prop_sig(), then prop_sig() will try to connect this changed() signal to
+the relevant <var>_changed property of the containing class.
+
+For example:
+
+	class Car(QObject):
+		changed= pyqtSignal()
+
+		driver, driver_changed = prop_sig(Person, "driver")
+
+		def __init__(self):
+			QObject.__init__(self)
+			driver_changed.connect(self.changed)
+	
+	car = Car()
+	car.driver.age = 20
+
+The following signals will be emitted after the last line:
+	car.driver.age_changed(20)
+	car.driver.changed()
+	car.driver_changed(driver)
+	car.changed()
+"""
+
+def try_connect_signals(self, mem, sig):
+	"""Private!!"""
+	"""
+	Try to connect the changed() signal of an inner class to the *_changed() signal of the outer class.
+
+	BUG: we don't store the lambda function, so there's no way of disconnecting the signal
+	when the object is replaced. A change to the previous object would then trigger the *_changed()
+	signal of the outer class.
+
+	This bug is not too bad, since overemitting *_changed() shouldn't change behaviour.
+	"""
+	try:
+		self.__getattribute__(mem).changed.connect(
+				lambda: self.__getattribute__(sig).emit(self.__getattribute__(mem)))
+	except AttributeError:
+		# Ignore if member doesn't have a changed() signal
+		pass
+
+def get_fget(mem, type, sig, default):
+	"""Private!!"""
 	def fget(self):
 		try:
 			self.__getattribute__(mem)
 		except AttributeError:
-#			Need to have the behaviour that an unset default gives
-#			no default value => don't special-case default==None.
-                        if default is None:
-                                self.__setattr__(mem, type())
-                        else:
-                                self.__setattr__(mem, default)
+			# Need to have the behaviour that an unset default gives
+			# a default value => special-case default==None.
+			if default is None:
+				self.__setattr__(mem, type())
+			else:
+				self.__setattr__(mem, default)
+			try_connect_signals(self, mem, sig)
 		return self.__getattribute__(mem)
 	return fget
 
 def get_fset(mem, type, sig):
+	"""Private!!"""
 	def fset(self, value):
 		try:
 			if self.__getattribute__(mem) == value:
@@ -25,6 +96,7 @@ def get_fset(mem, type, sig):
 		# delete() equivalent here if needed
 		# Filter value through new() here if needed
 		self.__setattr__(mem, value)
+		try_connect_signals(self, mem, sig)
 		# Emit the filtered value, not the given value
 		self.__getattribute__(sig).emit(self.__getattribute__(mem))
 	return fset
@@ -39,18 +111,19 @@ def prop_sig(type, name, default=None, doc=None):
 	Example:
 		class cls(object):
 			length, length_changed = prop_sig(int, "length", 20)
-	
-	The new method should take one argument, the object that is assigned
-	to the property. It acts as a filter before the assigned object is
-	actually set.
-
-	The delete method should take one argument, the object that is
-	to be deleted. It will be called on the property value when it is
-	about to be replaced by another value.
 	"""
+#	"""
+#	The new method should take one argument, the object that is assigned
+#	to the property. It acts as a filter before the assigned object is
+#	actually set.
+#
+#	The delete method should take one argument, the object that is
+#	to be deleted. It will be called on the property value when it is
+#	about to be replaced by another value.
+#	"""
 	sig = name + "_changed"
 	mem = "_" + name
-	fget = get_fget(mem, type, default)
+	fget = get_fget(mem, type, sig, default)
 	fset = get_fset(mem, type, sig)
 	return pyqtProperty(type, fget, fset, doc=doc), pyqtSignal(type)
 
@@ -83,9 +156,12 @@ def link(obj1, name1, obj2, name2):
 
 if __name__ == "__main__":
 	class abject(QObject):
+
+		length, length_changed = prop(int, "length", 1)
+
 		def __init__(self):
 			QObject.__init__(self)
-		length, length_changed = prop(int, "length", 1)
+			self.length_changed.connect(self.p)
 
 		@staticmethod
 		def p(x):
--- style/style.py
+++ style/style.py
@@ -1,5 +1,5 @@
-from PyQt4.QtGui import QFont, QColor
-from PyQt4.QtCore import Qt
+from PyQt4.QtGui import QFont, QColor, QPen, QBrush
+from PyQt4.QtCore import QObject, Qt, pyqtSignal, pyqtProperty
 
 from base.property import prop_sig
 
@@ -7,6 +7,14 @@ from base.property import prop_sig
 # QFont and QPen. QPen is for colour, QFont for all else.
 # However, QColor can be used in place of QPen.
 
-class TextStyle(object):
+class TextStyle(QObject):
+	changed = pyqtSignal()
+
 	font, font_changed = prop_sig(QFont, "font")
 	colour, colour_changed = prop_sig(QColor, "colour", Qt.black)
+
+	def __init__(self):
+		QObject.__init__(self)
+		for signal in self.font_changed, self.colour_changed:
+			signal.connect(self.changed)
+
